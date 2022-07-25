d4b5cce5cdaddfcbc9e1410f193d18623449e696
Sito8943 <sito8943@gmail.com>
2022-07-25 16:24:14 -0700

    doing animations

--- src/layouts/Services.jsx
+++ src/layouts/Services.jsx
@@ -35,10 +35,15 @@ const Projects = () => {
     <div
       id="section-services"
       className="uk-section uk-section-muted uk-section-large uk-padding-large"
+      data-uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 500;"
     >
       <div className="uk-container">
-        <h2>{languageState.texts.Projects.Title}</h2>
-        <p>{languageState.texts.Hero.Description}</p>
+        <h2 className="uk-h2" data-uk-scrollspy="cls: uk-animation-fade;">
+          {languageState.texts.Projects.Title}
+        </h2>
+        <p data-uk-scrollspy="cls: uk-animation-fade;">
+          {languageState.texts.Hero.Description}
+        </p>
         <div
           class={`uk-child-width-1-1@s uk-child-width-1-2@m uk-child-width-expands@l uk-grid-match ${css(
             {
